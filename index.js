e1b4b96ee875bfac82eee7bf11526196afdd3ac5
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-16 21:56:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
 const message = `${name} is ${height} inches tall`;
 
-function doNothing() {}
-
-function sayHello() {
-  console.log("Hello!");
-}
-sayHello();
-
-function sayHelloToIsabel() {
-  console.log("Hello, Isabel!");
-}
-sayHelloToIsabel();
-
-function sayHelloToSofia() {
-  console.log("Hello, Sofia!");
-}
-sayHelloToSofia();
-
-function sayHelloToBrendan() {
-  console.log("Hello, Brendan!");
-}
-sayHelloToBrendan()
-
-module.exports = {name, height, message};
+module.exports = { name, height, message };
